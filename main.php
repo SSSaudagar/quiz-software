dbfbd6ab26d141c4764daa087ea900cef207da3e
Shashank Shetye Saudagar <shashanksaudagar@gmail.com>
2015-03-15 03:31:20 +0530

    frame setup

--- main.php
+++ main.php
@@ -26,7 +26,7 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
-            
+            <li><a href="#" class="navbar-brand"><span class="glyphicon glyphicon-log-out"> Log-Out</span></a>
           </ul>
         </div>
       </div>
@@ -46,8 +46,23 @@
             
           </ul>
         </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        <div class="col-sm-9 col-sm-offset-3 col-md-offset-2 col-md-8  main">
+            <div id="question" class="row">
+                
+            </div>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-4"><button class="btn btn-primary btn-block">&lt;- Prev</button></div>
+                <div class="col-md-1"></div>
+                <div class="col-md-1"></div>
+                <div class="col-md-4"><button class="btn btn-success btn-block">Next -&gt;</button></div>
+                <div class="col-md-1"></div>
+            </div>
         </div>
+          
+          <div class="col-md-2 col-md-offset-10 right-sidebar">
+            
+          </div>
       </div>
     </div>
 
